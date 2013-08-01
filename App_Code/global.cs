02a3c8de8677f99ec68778f8262ad213e8b73335
marc agas <marclambertagas@gmail.com>
2013-08-01 13:12:52 +0800

    Global declaration

---
+++ App_Code/global.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for global
+/// </summary>
+/// 
+
+
+public class global
+{
+    public Boolean loggedIn = false;
+
+	public global()
+	{
+        
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
