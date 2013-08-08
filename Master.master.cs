a3b66533bbe76a8b00d649dc66256a982687eaa9
marc agas <marclambertagas@gmail.com>
2013-08-09 05:52:48 +0800

    WIP mycart

--- Master.master.cs
+++ Master.master.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Web.Security;
+using System.Data;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Text;
 
 public partial class Master : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (Session["logged_in_info"] == null)
+        {
+            //Response.Cookies["UserId"].Expires = DateTime.Now.AddDays(-1);
+            //Response.Cookies["FirstName"].Expires = DateTime.Now.AddDays(-1);
+        }
     }
 }
